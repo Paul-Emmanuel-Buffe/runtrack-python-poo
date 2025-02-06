69dabcf333601bcd51b3ec38b5eaa8e54973fdfa
unknown <paul-emmanuel.buffe@laplateforme.io>
2025-02-06 14:41:50 +0100

    Job 01 finished

---
+++ Jour_04/Job_01.py
@@ -0,0 +1,35 @@
+class Personne:
+    def __init__(self, age):
+        self.age = 14
+
+    def afficherAge (self):
+         print(self.age)
+
+    def bonjour (self):
+        print("Hello")
+
+    def modifier_age (self):
+        self.age = int(input("Quel age ? : "))
+
+class Eleve (Personne):
+    def __init__(self, age):
+        super().__init__(age)
+   
+    def aller_cours (self):
+        print("Je vais en cours")
+
+    def afficher_age (self):
+        print(f"J'ai {self.age} ans.")
+
+class Professeur:
+    def __init__ (self, matière_enseignée):
+        self._matière_enseignée = matière_enseignée
+
+    def enseigner (self):
+        print("Le cours va commencer")
+
+
+# Début des essais
+paul = Eleve(Personne)
+paul.aller_cours()
+paul.afficher_age()
